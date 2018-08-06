53ef9f98ed9b3ce3060d74b4752e108f397b0760
dingmouren <naildingmouren@gmail.com>
2018-08-06 18:30:17 +0800

    merge

---
+++ app/src/main/java/com/example/application/ui/home/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.application.ui.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.application.R;
+
+/**
+ * Created by dingmouren
+ * email: naildingmouren@gmail.com
+ * github: https://github.com/DingMouRen
+ */
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+        return rootView;
+    }
+}
